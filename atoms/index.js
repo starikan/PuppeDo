e09d22cedce01d21995a2517b9d8a58c7aa879c0
sstarodubov <s.starodubov@litebox.ru>
2018-07-17 15:57:41 +0400

    Autogenerate atoms index

--- atoms/index.js
+++ atoms/index.js
@@ -1,9 +1,25 @@
-const { log } = require('../logger/logger');
+const _ = require('lodash');
+const walkSync = require('walk-sync');
 
-module.exports = {
-  'typeInput': require('./typeInput'),
-  'buttonClick': require('./buttonClick'),
-  'goTo': require('./goTo'),
-  'checkSelector': require('./checkSelector'),
-  'if': require('./if'),
-};
+let atoms = {};
+var paths = walkSync('./atoms');
+
+paths.forEach(p => {
+  let nameFile = p.split('.')[0];
+  if (nameFile && nameFile != 'index'){
+    console.log(nameFile)
+    atoms[nameFile] = require('./' + nameFile);
+  }
+});
+
+console.log(atoms)
+
+module.exports = atoms;
+
+// module.exports = {
+//   'typeInput': require('./typeInput'),
+//   'buttonClick': require('./buttonClick'),
+//   'goTo': require('./goTo'),
+//   'checkSelector': require('./checkSelector'),
+//   'if': require('./if'),
+// };
