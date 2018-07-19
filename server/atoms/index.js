02a38ed60ec0fefc392741deeb6c0d49e84d2cf9
sstarodubov <s.starodubov@litebox.ru>
2018-07-19 08:54:48 +0400

    Server and front separate

--- server/atoms/index.js
+++ server/atoms/index.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const walkSync = require('walk-sync');
 
 let atoms = {};
-var paths = walkSync('./atoms');
+var paths = walkSync('./server/atoms');
 
 paths.forEach(p => {
   let nameFile = p.split('.')[0];
--- server/env.js
+++ server/env.js
@@ -154,7 +154,7 @@ class Envs {
     const folder = path.join(output, `/${test}_${now}`);
     await fs.mkdirSync(folder);
     
-    fs.createReadStream('./logger/output.html').pipe(fs.createWriteStream(path.join(folder, 'output.html')));
+    fs.createReadStream('./server/logger/output.html').pipe(fs.createWriteStream(path.join(folder, 'output.html')));
 
     this.output.output = output;
     this.output.name = test;
