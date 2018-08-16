29f0c5da82662c7f8797e720cb6c01e6a17b2d3b
sstarodubov <s.starodubov@litebox.ru>
2018-08-16 15:04:37 +0400

    Remove server

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-let { print } = require('./testAsync')(123321)
+const { getFullDepthJSON } = require('./yaml/getFullDepthJSON');
+const { getTest } = require('./yaml/getTest');
 
-print();
+const main = async (args = {}) => {
+
+  process.on('unhandledRejection', async (error, p) => {
+    console.log('unhandledRejection')
+    console.log(error, p)
+    debugger
+    process.exit(1);
+  });
+
+  const { envsId, envs, log } = require('./env')();
+  const debugOnError = true;
+  if (debugOnError){
+    envs.set('debugOnError', debugOnError);
+  }
+
+  await envs.init(args);
+  const full = await getFullDepthJSON({
+    envs: envs,
+    filePath: envs.get('args.testFile'),
+  });
+  let test = getTest(full, envsId);
+  await test();
+  await envs.closeBrowsers()
+}
+
+exports.main = main;
+
+// try {
+//   main();
+// }
+// catch (error) {}
