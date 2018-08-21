7860dcab2ba002ec41e79db451a42f768f48ef24
sstarodubov <s.starodubov@litebox.ru>
2018-08-21 12:54:15 +0400

    DEBUG_MODE option

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const main = async (args = {}) => {
   process.on('unhandledRejection', async (error, p) => {
     console.log('unhandledRejection')
     console.log(error, p)
-    debugger
+    if (process.env.PPD_DEBUG_MODE) debugger;
     process.exit(1);
   });
 
   const { envsId, envs, log } = require('./env')();
-  const debugOnError = true;
-  if (debugOnError){
-    envs.set('debugOnError', debugOnError);
-  }
 
   await envs.init(args);
   const full = await getFullDepthJSON({
