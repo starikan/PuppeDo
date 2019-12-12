80c0f5372fde2e970927f81b75c5fc511f9047ab
Sergey Starodubov <4analit@gmail.com>
2019-12-12 11:36:14 +0400

    Resolve atoms without runTest section

--- getTest.js
+++ getTest.js
@@ -3,74 +3,61 @@ const path = require('path');
 const _ = require('lodash');
 
 const abstractTest = require('./abstractTest');
+const RUNNER_BLOCK_NAMES = ['beforeTest', 'runTest', 'afterTest', 'errorTest'];
 
 const getTest = function(testJsonIncome, envsId, socket) {
   const { envs } = require('./env')({ envsId, socket });
 
-  let testJson = {...testJsonIncome};
+  let testJson = { ...testJsonIncome };
   if (!testJson || !_.isObject(testJson) || !envsId) {
     throw { message: 'getTest params error' };
   }
-  const functions = _.pick(testJson, ['beforeTest', 'runTest', 'afterTest', 'errorTest']);
+  const functions = _.pick(testJson, RUNNER_BLOCK_NAMES);
 
   // Pass source code of test into test for logging
-  testJson.source = {...testJsonIncome};
+  testJson.source = { ...testJsonIncome };
 
   testJson.socket = socket;
 
-  for (let funcKey in functions) {
-    testJson[funcKey] = [];
-    let funcVal = functions[funcKey];
+  // If there is no any function in test we deside that it have runTest in js file with the same name
+  if (!Object.keys(functions).length && ['atom'].includes(testJson.type)) {
+    const funcKey = 'runTest';
+    const testFileExt = path.parse(testJson.filePath).ext;
+    const funcFile = path.resolve(testJson.filePath.replace(testFileExt, '.js'));
+    try {
+      const funcFromFile = _.get(require(funcFile), funcKey);
+      testJson.funcFile = path.resolve(funcFile);
+      testJson[funcKey] = [funcFromFile];
+    } catch (error) {
+      // If there is no JS file it`s fine.
+    }
+  } else {
+    for (let funcKey in functions) {
+      let funcVal = functions[funcKey];
 
-    if (_.isArray(funcVal)) {
-      for (let test of funcVal) {
-        if (['test', 'atom'].includes(test.type)) {
-          testJson[funcKey].push(getTest(test, envsId, socket));
+      // Resolve nested
+      if (_.isArray(funcVal)) {
+        testJson[funcKey] = [];
+        for (let test of funcVal) {
+          if (['test', 'atom'].includes(test.type)) {
+            testJson[funcKey].push(getTest(test, envsId, socket));
+          }
         }
       }
-    }
 
-    if (_.isString(funcVal)) {
-      let funcFile = path.join(envs.get('args.testsFolder'), funcVal);
-      try {
+      if (_.isString(funcVal)) {
+        let funcFile = path.resolve(path.join(envs.get('args.testsFolder'), funcVal));
         try {
           funcFromFile = _.get(require(funcFile), funcKey);
+          if (_.isFunction(funcFromFile)) {
+            testJson.funcFile = path.resolve(funcFile);
+            testJson[funcKey] = [funcFromFile];
+          }
         } catch (err) {
-          // if relative path of testFolder
-          funcFile = path.join(process.cwd(), funcFile);
-          funcFromFile = _.get(require(funcFile), funcKey);
-        }
-        if (_.isFunction(funcFromFile)) {
-          // Resolve JS file for test for logging
-          testJson.funcFile = path.resolve(funcFile);
-          testJson[funcKey] = [funcFromFile];
-        } else {
           throw {
             message: `Функция по ссылке не найдена ${funcKey} -> ${funcVal}, файл ${funcFile}. Проверьте наличии функции и пути.`,
           };
         }
-      } catch (err) {
-        console.log(err);
-        throw {
-          message: `Функция по ссылке не доступна ${funcKey} -> ${funcVal}, файл ${funcFile}. Проверьте наличии функции и пути.`,
-        };
-      }
-    }
-
-    // TODO: Сделать чтобы runTest убрать из атомов а если есть то искать js которая указана а если нет типа blank чтоб ничего не делало
-
-    if (!funcVal) {
-      // debugger
-      const testFileExt = path.parse(testJson.testFile).ext;
-      const funcFile = testJson.testFile.replace(testFileExt, '.js');
-      try {
-        funcFromFile = _.get(require(funcFile), funcKey);
-        testJson.funcFile = path.resolve(funcFile);
-        testJson[funcKey] = [funcFromFile];
-      } catch (error) {
-        throw {
-          message: `Функция по ссылке не найдена ${funcKey}, файл ${funcFile}. Проверьте наличии функции и пути.`,
-        };
       }
     }
   }
