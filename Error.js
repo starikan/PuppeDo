742f7fe5368a8f3b075814174670a4fd01736650
Sergey Starodubov <4analit@gmail.com>
2019-12-20 11:15:53 +0400

    Refactoring

---
+++ Error.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+const { stylesConsole } =  require('./helpers');
+
+const errorHandler = async error => {
+  error.messageObj = _.get(error, 'message').split(' || ');
+  if (error.socket && error.socket.sendYAML) {
+    error.socket.sendYAML({ data: { ...error }, type: error.type || 'error', envsId: error.envsId });
+  }
+  if (error.stack) {
+    error.stack = error.stack.split('\n    ');
+  }
+  if (error.debug) {
+    debugger;
+  }
+  const styleFunction = get(stylesConsole, 'trace', args => args);
+  console.log(styleFunction(error.message));
+  console.log();
+  console.log(styleFunction(error.stack));
+  if (!module.parent) {
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  errorHandler,
+};
