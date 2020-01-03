7892574344057c68b79960abd314af5da43dbb53
Sergey Starodubov <4analit@gmail.com>
2020-01-03 15:04:40 +0400

    Tests with atoms repo

--- tests/atoms/goTo.js
+++
@@ -1,9 +0,0 @@
-const instance = new (require('./Atom'))();
-module.exports = { runTest: instance.runTest.bind(instance) };
-
-// WRITE YOUR LOGIC BELLOW
-instance.atomRun = async function() {
-  const { url } = this.data;
-  await this.page.goto(url);
-  this.log({ text: `Go to: ${url}` });
-};
