565e35b0498374932e06931e46a2bdf4b18e05b8
Sergey Starodubov <4analit@gmail.com>
2020-01-06 18:42:48 +0400

    Refactoring atoms import

--- tests/tests/cdp.js
+++ tests/tests/cdp.js
@@ -1,91 +1,10 @@
-class Atom {
-  constructor() {}
-
-  async getElement(page, selector, allElements = false) {
-    if (page && selector && typeof selector === 'string' && typeof page === 'object') {
-      let element;
-      if (selector.startsWith('xpath:')) {
-        selector = selector.replace(/^xpath:/, '');
-        element = await page.$x(selector);
-        if (!allElements) {
-          if (element.length > 1) {
-            throw { message: `Finded more then 1 xpath elements ${selector}` };
-          }
-          element = element[0];
-        }
-      } else {
-        selector = selector.replace(/^css:/, '');
-        element = allElements ? await page.$$(selector) : await page.$(selector);
-      }
-      return element;
-    } else {
-      return false;
-    }
-  }
-
-  async atomRun() {
-    console.log('Empty Atom Run');
-  }
-
-  async runTest(args = {}) {
-    try {
-      const startTime = new Date();
-
-      this.envs = args.envs;
-      this.envsId = args.envsId;
-      this.envName = args.envName;
-      this.envPageName = args.envPageName;
-      this.data = args.data;
-      this.selectors = args.selectors;
-      this.options = args.options;
-      this.allowResults = args.allowResults;
-      this.bindResults = args.bindResults;
-      this.levelIndent = args.levelIndent;
-      this.repeat = args.repeat;
-      this.stepId = args.stepId;
-      this.env = args.env;
-      this.browser = args.browser;
-      this.page = args.page;
-      // TODO: delete this arter refactoring
-      this.helper = args.helper;
-      this._ = args._;
-      this.name = args.name;
-      this.description = args.description;
-
-      this.screenshot = (this.options || {})['screenshot'] || false;
-      this.fullpage = (this.options || {})['fullpage'] || false;
-      this.level = (this.options || {})['level'] || 'raw';
-      this.log = function(cusomLog) {
-        args.log({
-          ...{
-            screenshot: this.screenshot,
-            fullpage: this.fullpage,
-            level: this.level,
-            levelIndent: this.levelIndent + 1,
-          },
-          ...cusomLog,
-        });
-      };
-
-      await this.atomRun();
-
-      const timer = (this.envs.args || {})['PPD_LOG_TIMER'] || false;
-      if (timer) {
-        console.log(`${' '.repeat(21)}${' | '.repeat(this.levelIndent + 1)} ⌛: ${new Date() - startTime} ms.`);
-      }
-    } catch (error) {
-      debugger;
-      throw { message: `Error in Atom` };
-    }
-  }
-}
-
+const { Atom } = require('@puppedo/atoms');
 const instance = new Atom();
 module.exports = { runTest: instance.runTest.bind(instance) };
 
 // WRITE YOUR LOGIC BELLOW
 instance.atomRun = async function() {
-  const js = () => {
+  const jsEvalOnClick = () => {
     function clickHandler(event) {
       console.log(event);
 
@@ -162,7 +81,6 @@ instance.atomRun = async function() {
 
       console.log(exportData);
       console.log(JSON.stringify(exportData, { skipInvalid: true }));
-      // debugger;
     }
     window.addEventListener('click', clickHandler, true);
     return window;
@@ -173,20 +91,20 @@ instance.atomRun = async function() {
 
   await this.page.addScriptTag({ url: yamlFile });
   await this.page.addScriptTag({ url: lodashFile });
-  await this.page.evaluate(js);
+  await this.page.evaluate(jsEvalOnClick);
 
   const client = await this.page.target().createCDPSession();
   await client.send('Console.enable');
   await client.send('DOM.enable');
   client.on('Console.messageAdded', async e => {
     const textLog = e.message.text;
-    console.log(textLog);
     try {
       const data = JSON.parse(textLog);
-      const { x, y } = data;
-      const { nodeId } = await client.send('DOM.getNodeForLocation', { x, y });
-      const nodeIdDescribe = await client.send('DOM.describeNode', { nodeId });
-      debugger;
+      // const { x, y } = data;
+      // const { nodeId } = await client.send('DOM.getNodeForLocation', { x, y });
+      // const nodeIdDescribe = await client.send('DOM.describeNode', { nodeId });
+      // debugger;
+      console.log(data)
     } catch (err) {
       // debugger;
     }
