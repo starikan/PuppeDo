59059b319a243206f2f778c818b2e3a9a708e4d5
Sergey Starodubov <4analit@gmail.com>
2020-01-30 12:19:23 +0400

    Helpers specs

---
+++ helpers.spec.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+
+const { merge, sleep, stylesConsole, blankSocket } = require('./helpers');
+
+test('Helpers -> merge', () => {
+  const foobar = { foo: { bar: 3 } };
+  const foobaz = { foo: { baz: 4 } };
+  const bar = { bar: 'yay!' };
+  expect(merge(foobar, foobaz, bar)).toEqual({ foo: { bar: 3, baz: 4 }, bar: 'yay!' });
+  expect(merge([1, 2, 3], [3, 2, 1])).toEqual([3, 2, 1]);
+});
+
+test('Helpers -> sleep', () => {});
+
+test('Helpers -> stylesConsole', () => {});
+
+test('Helpers -> blankSocket', () => {
+  expect(Object.keys(blankSocket)).toEqual(['send','sendYAML'])
+  expect(_.isFunction(blankSocket.send)).toBe(true)
+  expect(_.isFunction(blankSocket.sendYAML)).toBe(true)
+});
