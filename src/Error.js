07284c2428124b20e3088779295480cad643bf8f
Sergey Starodubov <4analit@gmail.com>
2020-02-18 21:10:54 +0400

    Refactoring

--- src/Error.js
+++ src/Error.js
@@ -1,15 +1,18 @@
-const { paintString } = require('./helpers');
+const { paintString } = require('./Helpers.js');
 
-const errorHandler = async (error) => {
+const errorHandler = async (errorIncome) => {
+  const error = { ...errorIncome };
   error.messageObj = (error.message || '').split(' || ');
   if (error.socket && error.socket.sendYAML) {
     error.socket.sendYAML({ data: { ...error }, type: error.type || 'error', envsId: error.envsId });
   }
   if (error.stack) {
     error.stack = error.stack.split('\n    ');
+    // eslint-disable-next-line no-console
     console.log(paintString(error.stack, 'error'));
   }
   if (error.debug) {
+    // eslint-disable-next-line no-debugger
     debugger;
   }
   // if (!module.parent) {
