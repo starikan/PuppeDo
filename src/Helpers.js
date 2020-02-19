fd62cc7cc62752f2a0696e7829bb818a7a407ea8
Sergey Starodubov <4analit@gmail.com>
2020-02-19 14:51:42 +0400

    Refactoring

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,33 +1,37 @@
 const deepmerge = require('deepmerge');
 
-const sleep = (ms) => new Promise((resolve) => {
+function sleep(ms) {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+}
 
-const merge = (...objects) => deepmerge.all(objects, { arrayMerge: (destinationArray, sourceArray, options) => sourceArray });
+const merge = (...objects) => deepmerge.all(objects, { arrayMerge: (destinationArray, sourceArray) => sourceArray });
 
-// https://stackoverflow.com/questions/23975735/what-is-this-u001b9-syntax-of-choosing-what-color-text-appears-on-console
+/*
+https://stackoverflow.com/questions/23975735/what-is-this-u001b9-syntax-of-choosing-what-color-text-appears-on-console
 
-// SANE = "\u001B[0m"
+SANE = "\u001B[0m"
 
-// HIGH_INTENSITY = "\u001B[1m"
-// LOW_INTENSITY = "\u001B[2m"
+HIGH_INTENSITY = "\u001B[1m"
+LOW_INTENSITY = "\u001B[2m"
 
-// ITALIC = "\u001B[3m"
-// UNDERLINE = "\u001B[4m"
-// BLINK = "\u001B[5m"
-// RAPID_BLINK = "\u001B[6m"
-// REVERSE_VIDEO = "\u001B[7m"
-// INVISIBLE_TEXT = "\u001B[8m"
+ITALIC = "\u001B[3m"
+UNDERLINE = "\u001B[4m"
+BLINK = "\u001B[5m"
+RAPID_BLINK = "\u001B[6m"
+REVERSE_VIDEO = "\u001B[7m"
+INVISIBLE_TEXT = "\u001B[8m"
 
-// BACKGROUND_BLACK = "\u001B[40m"
-// BACKGROUND_RED = "\u001B[41m"
-// BACKGROUND_GREEN = "\u001B[42m"
-// BACKGROUND_YELLOW = "\u001B[43m"
-// BACKGROUND_BLUE = "\u001B[44m"
-// BACKGROUND_MAGENTA = "\u001B[45m"
-// BACKGROUND_CYAN = "\u001B[46m"
-// BACKGROUND_WHITE = "\u001B[47m"
+BACKGROUND_BLACK = "\u001B[40m"
+BACKGROUND_RED = "\u001B[41m"
+BACKGROUND_GREEN = "\u001B[42m"
+BACKGROUND_YELLOW = "\u001B[43m"
+BACKGROUND_BLUE = "\u001B[44m"
+BACKGROUND_MAGENTA = "\u001B[45m"
+BACKGROUND_CYAN = "\u001B[46m"
+BACKGROUND_WHITE = "\u001B[47m"
+*/
 
 const paintString = (str, color = 'sane') => {
   const colors = {
--- src/Log.js
+++ src/Log.js
@@ -7,62 +7,10 @@ require('array-flat-polyfill');
 const dayjs = require('dayjs');
 const yaml = require('js-yaml');
 
-const { sleep, paintString } = require('./helpers');
-const { Arguments } = require('./Arguments');
+const { paintString } = require('./Helpers.js');
+const { Arguments } = require('./Arguments.js');
+const { Screenshot } = require('./Screenshot.js');
 const Environment = require('./Environment.js');
-
-class Screenshot {
-  constructor({ envsId } = {}) {
-    const { socket, envs } = Environment({ envsId });
-    this.envs = envs;
-    this.socket = socket;
-  }
-
-  async getScreenshots(element, fullPage = false, extendInfo = false) {
-    if (extendInfo) {
-      return [];
-    }
-    const elementScreenshot = await this.saveScreenshot({ element });
-    const fullPageScreenshot = await this.saveScreenshot({ fullPage });
-    const screenshotsExists = [elementScreenshot, fullPageScreenshot].filter((v) => v);
-    return screenshotsExists;
-  }
-
-  copyScreenshotToLatest(name) {
-    const { folder, folderLatest } = this.envs.getOutputsFolders();
-    const pathScreenshot = path.join(folder, name);
-    const pathScreenshotLatest = path.join(folderLatest, name);
-    fs.copyFileSync(pathScreenshot, pathScreenshotLatest);
-  }
-
-  async saveScreenshot({ fullPage = false, element = false } = {}) {
-    const { folder } = this.envs.getOutputsFolders();
-    try {
-      const name = `${dayjs().format('YYYY-MM-DD_HH-mm-ss.SSS')}.png`;
-      const pathScreenshot = path.join(folder, name);
-
-      if (fullPage) {
-        const page = this.envs.getActivePage();
-        await page.screenshot({ path: pathScreenshot, fullPage });
-      }
-
-      if (element && _.isObject(element) && !_.isEmpty(element)) {
-        await element.screenshot({ path: pathScreenshot });
-      }
-
-      if (fs.existsSync(pathScreenshot)) {
-        this.copyScreenshotToLatest(name);
-        await sleep(25);
-        return name;
-      }
-      return false;
-    } catch (err) {
-      err.message += ' || saveScreenshot selectors';
-      err.socket = this.socket;
-      throw err;
-    }
-  }
-}
 
 class Log {
   constructor({ envsId } = {}) {
@@ -292,4 +240,4 @@ class Log {
   }
 }
 
-module.exports = { Log, Screenshot };
+module.exports = { Log };
