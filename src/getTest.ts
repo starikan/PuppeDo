0b6c09519767138a6ec19f807f4bbcdf1da7cc4b
Sergey Starodubov <4analit@gmail.com>
2020-11-23 09:17:17 +0400

    inlineJS in ane test

--- src/getTest.ts
+++ src/getTest.ts
@@ -22,30 +22,31 @@ const resolveJS = (testJson: TestExtendType): TestExtendType => {
   const testJsonNew = { ...testJson };
 
   const functions = pick(testJsonNew, RUNNER_BLOCK_NAMES);
-  if (Object.keys(functions).length || testJsonNew.type !== 'atom') {
-    return testJsonNew;
+  if (Object.keys(functions).length && !testJsonNew.inlineJS) {
+    return testJson;
   }
-
-  const testFileExt = path.parse(testJsonNew.testFile).ext;
-  const funcFile = path.resolve(testJsonNew.testFile.replace(testFileExt, '.js'));
 
   try {
     if (testJsonNew.inlineJS && typeof testJsonNew.inlineJS === 'string') {
       try {
-        atoms[funcFile] = requireFromString(`module.exports = async function atomRun() {\n${testJsonNew.inlineJS}};`);
+        atoms[testJsonNew.name] = requireFromString(
+          `module.exports = async function atomRun() {\n${testJsonNew.inlineJS}};`,
+        );
       } catch (error) {
         error.message = `Some errors in inlineJS: ${testJsonNew.inlineJS}`;
         throw error;
       }
     } else {
-      atoms[funcFile] = atoms[funcFile] || __non_webpack_require__(funcFile);
+      const testFileExt = path.parse(testJsonNew.testFile).ext;
+      const funcFile = path.resolve(testJsonNew.testFile.replace(testFileExt, '.js'));
+      atoms[testJsonNew.name] = atoms[testJsonNew.name] || __non_webpack_require__(funcFile);
       testJsonNew.funcFile = path.resolve(funcFile);
     }
 
     const instance = new Atom();
-    instance.atomRun = atoms[funcFile];
+    instance.atomRun = atoms[testJsonNew.name];
 
-    if (typeof atoms[funcFile] === 'function') {
+    if (typeof atoms[testJsonNew.name] === 'function') {
       testJsonNew.runTest = [instance.runTest.bind(instance)];
     }
   } catch (error) {
@@ -88,19 +89,19 @@ const getTest = ({
 }): { test: TestLifecycleFunctionType } => {
   let testJson = JSON.parse(JSON.stringify(testJsonIncome));
 
-  const functions = {
-    beforeTest: testJson.beforeTest,
-    runTest: testJson.runTest,
-    afterTest: testJson.afterTest,
-  };
-  Object.keys(functions).forEach((funcBlock) => {
-    if (functions[funcBlock] && !Array.isArray(functions[funcBlock])) {
+  RUNNER_BLOCK_NAMES.forEach((funcBlock) => {
+    if (testJson[funcBlock] && !Array.isArray(testJson[funcBlock])) {
       throw new Error(`Block ${funcBlock} must be array. Path: '${testJson.breadcrumbs.join(' -> ')}'`);
     }
   });
 
-  testJson = resolveJS(testJson);
+  const functionsBeforeResolve = {
+    beforeTest: testJson.beforeTest,
+    runTest: testJson.runTest,
+    afterTest: testJson.afterTest,
+  };
 
+  testJson = resolveJS(testJson);
   testJson.envsId = envsId;
   testJson.socket = socket;
 
@@ -109,9 +110,9 @@ const getTest = ({
   // Test
   // blocker.push({ stepId: testJson.stepId, block: true, breadcrumbs: testJson.breadcrumbs });
 
-  Object.keys(functions).forEach((funcKey) => {
-    const funcVal = functions[funcKey];
-    if (funcVal) {
+  RUNNER_BLOCK_NAMES.forEach((funcKey) => {
+    const funcVal = functionsBeforeResolve[funcKey];
+    if (funcVal && !testJson.inlineJS) {
       testJson[funcKey] = [];
       funcVal.forEach((testItem: TestType) => {
         if (['test', 'atom'].includes(testItem.type)) {
