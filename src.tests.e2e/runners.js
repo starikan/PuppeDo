e4fab42144de19c1f0304f45f2164fb6355ea1f5
Sergey Starodubov <4analit@gmail.com>
2021-12-16 10:44:32 +0400

    e2e all main tests

--- src.tests.e2e/runners.js
+++ src.tests.e2e/runners.js
@@ -42,6 +42,15 @@ const testsE2E = {
       'PPD_LOG_FULLPAGE=true',
     ],
   },
+  mainPuppeteer: {
+    env: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'main',
+      PPD_DATA: '{ "myEnv": "mainEnv_puppeteer" }',
+      PPD_LOG_LEVEL_NESTED: 1,
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
   dataCheck: {
     params: {
       PPD_ROOT: 'tests',
@@ -70,6 +79,75 @@ const testsE2E = {
       PPD_ROOT_ADDITIONAL: [path.join(__dirname, '..', 'node_modules\\@puppedo\\atoms')],
       PPD_TESTS: 'blankEnv',
     },
+  },
+  atoms: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'atomsTest',
+      PPD_DATA: { myEnv: 'mainEnv' },
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  tags: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'tagsTest',
+      PPD_DATA: { myEnv: 'mainEnv' },
+      PPD_TAGS_TO_RUN: 'testTag',
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  descriptions: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'descriptionTest',
+      PPD_DATA: { myEnv: 'mainEnv' },
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  logs: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'logOptions',
+      PPD_DATA: { myEnv: 'mainEnv' },
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  continueOnError: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'continueOnError',
+      PPD_DATA: { myEnv: 'mainEnv' },
+      PPD_CONTINUE_ON_ERROR_ENABLED: true,
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  breakParentIfResult: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'breakParentIfResult',
+      PPD_DATA: { myEnv: 'mainEnv' },
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  recursion: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'recursion',
+      PPD_DATA: { myEnv: 'mainEnv' },
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
+  },
+  screenshots: {
+    params: {
+      PPD_ROOT: 'tests',
+      PPD_TESTS: 'screenshots',
+      PPD_DATA: { myEnv: 'mainEnv' },
+      PPD_LOG_EXTEND: true,
+      PPD_LOG_SCREENSHOT: true,
+      PPD_LOG_FULLPAGE: true,
+    },
+    runBeforeTest: () => require('@puppedo/atoms'),
   },
 };
 
